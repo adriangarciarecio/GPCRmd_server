53c7ef5caf585be9b4b518f963874ed62877edcb
adriangarciarecio <adrian.garcia.recio@gmail.com>
2023-01-11 14:45:50 +0100

    Update packages.sh

--- files/packages.sh
+++ files/packages.sh
@@ -1,1 +1,49 @@
-pip install django-sendfile djangorestframework django-rest-swagger django-graphos django-revproxy django-debug-toolbar django-utils-six django-haystack psycopg2-binary PyYAML biopython matplotlib mdtraj rdkit defusedxml pandas bokeh plotly pysolr XlsxWriter xlrd mdsrv pgadmin4 Cmake -y
+echo "Installing Django"
+echo "Installing Dj-sendfile"
+pip install django-sendfile -y
+echo "Installing Dj-restframework"
+pip install djangorestframework -y
+echo "Installing Dj-restswagger"
+pip install django-rest-swagger -y
+echo "Installing Dj-graphos"
+pip install django-graphos -y
+echo "Installing Dj-revproxy"
+pip install django-revproxy -y
+echo "Installing Dj-debugtoolbar"
+pip install django-debug-toolbar -y
+echo "Installing Dj-utilsix"
+pip install django-utils-six -y
+echo "Installing Dj-haystack"
+pip install django-haystack -y
+echo "Installing psycopg2"
+pip install psycopg2-binary -y
+echo "Installing PyYAML"
+pip install PyYAML -y
+echo "Installing biopython"
+pip install biopython -y
+echo "Installing matplotlib"
+pip install matplotlib -y
+echo "Installing mdtraj"
+pip install mdtraj -y
+echo "Installing rdkit"
+pip install rdkit -y
+echo "Installing defusedxml"
+pip install defusedxml -y
+echo "Installing pandas"
+pip install pandas -y
+echo "Installing bokeh"
+pip install bokeh -y
+echo "Installing plotly"
+pip install plotly -y
+echo "Installing pysolr"
+pip install pysolr -y
+echo "Installing xlsxWriter"
+pip install XlsxWriter -y
+echo "Installing xlrd"
+pip install xlrd -y
+echo "Installing mdsrv"
+pip install mdsrv -y
+echo "Installing pgadmin4"
+pip install pgadmin4 -y
+echo "Installing Cmake"
+pip install Cmake -y
