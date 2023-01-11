f285f21852a1fa60557becea137638243cb9d077
adriangarciarecio <adrian.garcia.recio@gmail.com>
2023-01-11 15:07:37 +0100

    Update packages with style

--- files/packages.sh
+++ files/packages.sh
@@ -1,49 +1,54 @@
-echo "Installing Django"
-echo "Installing Dj-sendfile"
-pip install django-sendfile -y
-echo "Installing Dj-restframework"
-pip install djangorestframework -y
-echo "Installing Dj-restswagger"
-pip install django-rest-swagger -y
-echo "Installing Dj-graphos"
-pip install django-graphos -y
-echo "Installing Dj-revproxy"
-pip install django-revproxy -y
-echo "Installing Dj-debugtoolbar"
-pip install django-debug-toolbar -y
-echo "Installing Dj-utilsix"
-pip install django-utils-six -y
-echo "Installing Dj-haystack"
-pip install django-haystack -y
-echo "Installing psycopg2"
-pip install psycopg2-binary -y
-echo "Installing PyYAML"
-pip install PyYAML -y
-echo "Installing biopython"
-pip install biopython -y
-echo "Installing matplotlib"
-pip install matplotlib -y
-echo "Installing mdtraj"
-pip install mdtraj -y
-echo "Installing rdkit"
-pip install rdkit -y
-echo "Installing defusedxml"
-pip install defusedxml -y
-echo "Installing pandas"
-pip install pandas -y
-echo "Installing bokeh"
-pip install bokeh -y
-echo "Installing plotly"
-pip install plotly -y
-echo "Installing pysolr"
-pip install pysolr -y
-echo "Installing xlsxWriter"
-pip install XlsxWriter -y
-echo "Installing xlrd"
-pip install xlrd -y
-echo "Installing mdsrv"
-pip install mdsrv -y
-echo "Installing pgadmin4"
-pip install pgadmin4 -y
-echo "Installing Cmake"
-pip install Cmake -y
+# STYLE:
+bash style.sh
+
+echo "${Cyan}· Check upgrade of pip...${Color_Off}"
+pip install --upgrade pip
+echo "${Cyan}· Installing Django${Color_Off}"
+echo "${Cyan}· Installing Dj-sendfile${Color_Off}"
+pip install django-sendfile 
+echo "${Cyan}· Installing Dj-restframework${Color_Off}"
+pip install djangorestframework 
+echo "${Cyan}· Installing Dj-restswagger${Color_Off}"
+pip install django-rest-swagger 
+echo "${Cyan}· Installing Dj-graphos${Color_Off}"
+pip install django-graphos 
+echo "${Cyan}· Installing Dj-revproxy${Color_Off}"
+pip install django-revproxy 
+echo "${Cyan}· Installing Dj-debugtoolbar${Color_Off}"
+pip install django-debug-toolbar 
+echo "${Cyan}· Installing Dj-utilsix${Color_Off}"
+pip install django-utils-six 
+echo "${Cyan}· Installing Dj-haystack${Color_Off}"
+pip install django-haystack 
+echo "${Cyan}· Installing psycopg2${Color_Off}"
+pip install psycopg2-binary 
+echo "${Cyan}· Installing PyYAML${Color_Off}"
+pip install PyYAML 
+echo "${Cyan}· Installing biopython${Color_Off}"
+pip install biopython 
+echo "${Cyan}· Installing matplotlib${Color_Off}"
+pip install matplotlib 
+echo "${Cyan}· Installing mdtraj${Color_Off}"
+pip install mdtraj 
+echo "${Cyan}· Installing rdkit${Color_Off}"
+pip install rdkit 
+echo "${Cyan}· Installing defusedxml${Color_Off}"
+pip install defusedxml 
+echo "${Cyan}· Installing pandas${Color_Off}"
+pip install pandas 
+echo "${Cyan}· Installing bokeh${Color_Off}"
+pip install bokeh 
+echo "${Cyan}· Installing plotly${Color_Off}"
+pip install plotly 
+echo "${Cyan}· Installing pysolr${Color_Off}"
+pip install pysolr 
+echo "${Cyan}· Installing xlsxWriter${Color_Off}"
+pip install XlsxWriter 
+echo "${Cyan}· Installing xlrd${Color_Off}"
+pip install xlrd 
+echo "${Cyan}· Installing mdsrv${Color_Off}"
+pip install mdsrv 
+echo "${Cyan}· Installing pgadmin4${Color_Off}"
+pip install pgadmin4 
+echo "${Cyan}· Installing Cmake${Color_Off}"
+pip install Cmake 
